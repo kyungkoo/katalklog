3644bfe48c3e0821a0f9e2e019aee501d9bac900
Kyungkoo <kyungkoo.kang@yahoo.com>
2014-07-14 21:57:10 +0900

    modified for loop

--- main.py
+++ main.py
@@ -24,24 +24,22 @@ def file_upload():
   sorted_dic = katalklog.sorted_dict(data)
 
   if len(sorted_dic) > 20:
-
     result = dict()
-
     count = 0
-
     for key in sorted_dic:
       if count == 20:
         break
       result[key] = sorted_dic[key]
       count+=1
+    return render_template('result.html',datas=result)
   else:
-    return render_template('result.html',datas=sorted_dic)  
-
-  return render_template('result.html',datas=result)
+    return render_template('result.html',datas=sorted_dic)
 
 
 @app.route('/sample')
 def sample_page():
-
+  """
+  sample page for test pie chart and jinja2
+  """
   arrays = {'Hello':20,'thankyou':10,'GoodNight':5,'Loveyou':40,'no':2}
   return render_template('result.html',datas=arrays)
